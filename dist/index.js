cff38647e77a79ab1d66fcb88e00b3067400e817
Matthew Peveler <matt.peveler@gmail.com>
2021-03-19 22:12:10 -0400

    generate dist files
    Signed-off-by: Matthew Peveler <matt.peveler@popsql.com>

--- dist/index.js
+++ dist/index.js
@@ -7,24 +7,40 @@ require('./sourcemap-register.js');module.exports =
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
-const core_1 = __importDefault(__nccwpck_require__(186));
-const github_1 = __importDefault(__nccwpck_require__(438));
+const core = __importStar(__nccwpck_require__(186));
+const github = __importStar(__nccwpck_require__(438));
 const validate_1 = __nccwpck_require__(997);
 function run() {
-    if (!github_1.default.context.payload.pull_request) {
-        core_1.default.setFailed('This action can only be run on pull requests');
+    if (!github.context.payload.pull_request) {
+        core.setFailed('This action can only be run on pull requests');
         return;
     }
     try {
-        validate_1.checkTitle(github_1.default.context.payload.pull_request.title);
+        validate_1.checkTitle(github.context.payload.pull_request.title);
     }
     catch (exc) {
-        core_1.default.setFailed(exc.message);
+        core.setFailed(exc.message);
     }
 }
 exports.run = run;
